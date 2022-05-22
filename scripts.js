fef96bf8d400abc8a58e2f711ef3cf1b073f333b
miguel9364 <miguel68644794@hotmail.com>
2022-05-22 16:51:17 -0300

    Finalizado

--- scripts.js
+++ scripts.js
@@ -1,31 +1,16 @@
-    
-        function MudarCor(){
-        
-       Cor1(),Cor2(),Cor;
-       
-
         function Cor1(){
-        const click = document.querySelector("div#botao")
-        click.style.background="red"
-        document.body.style.background="yellow"
-        click.style.color="yellow"
-        click.addEventListener('click',Cor2)
+        document.body.style.background="darkorchid"
         }
 
         function Cor2(){
-        const click = document.querySelector("div#botao")
-        click.style.background="green"
-        document.body.style.background="red"
-        click.style.color="red"
-        click.addEventListener('click',Cor)
+        document.body.style.background="springgreen"
         }
-        function Cor(){
-        const click = document.querySelector("div#botao")
-        click.style.background="yellow"
-        document.body.style.background="green"
-        click.style.color="green"
-        click.addEventListener('click',Cor1)
+        function Cor3(){
+        document.body.style.background="burlywood"
         }
-    }
+        function Cor4(){
+        document.body.style.background="aqua"
+        }
+    
         
   
